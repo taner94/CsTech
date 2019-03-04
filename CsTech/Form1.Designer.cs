4bd2034015c205b6665e957c00fca7f7328d78c6
Taner Yıldırım <yldrmtnr@windowslive.com>
2019-03-04 16:59:53 +0300

    Add project files.

---
+++ CsTech/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CsTech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(27, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(211, 464);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(323, 105);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(187, 464);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kullanıcı Tahmin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Yazılım Tahmin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CsTech/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsTech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int sayi { get; set; }
+
+        private string RandomSayi()
+        {
+            int x; int y; int z; int t;
+            Random rastgele = new Random();
+            do
+            {
+                x = rastgele.Next(0, 9);
+
+                y = rastgele.Next(0, 9);
+
+                z = rastgele.Next(0, 9);
+
+                t = rastgele.Next(0, 9);
+
+            } while ((x != 0) && (x != y) && (x != z) && (x != t) && (y != z) && (y != t) && (z != t));
+             sayi =Convert.ToInt32( Convert.ToString(x) + Convert.ToString(y) + Convert.ToString(z) + Convert.ToString(t));
+
+            return Convert.ToString(sayi);
+        }
+
+
+        private void RandomSayiB()
+
+        {
+            int j = 0;
+            int x; int y; int z; int t;
+            Random rastgele = new Random();
+            do
+            {
+                x = rastgele.Next(0, 9);
+
+                y = rastgele.Next(0, 9);
+
+                z = rastgele.Next(0, 9);
+
+                t = rastgele.Next(0, 9);
+
+            } while ((x != 0) && (x != y) && (x != z) && (x != t) && (y != z) && (y != t) && (z != t));
+            string sayib =Convert.ToString(x) + Convert.ToString(y) + Convert.ToString(z) + Convert.ToString(t);
+         
+            string sayi2 = sayi.ToString();
+            char[] array = sayi2.ToCharArray();
+
+            char[] girilensayi = sayib.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                char array2 = array[i];
+                char girilensayi2 = girilensayi[i];
+
+                if (array[i] == girilensayi[i])
+                {
+                    listBox2.Items.Add((i + 1) + ". Harf Kontrol : +");
+
+                    j++;
+                    if (j == 4)
+                    {
+                        listBox2.Items.Add("Doğru Bildiniz");
+                    }
+
+                }
+                else
+                {
+                    listBox2.Items.Add((i + 1) + ". Harf Kontrol : -");
+                }
+            }
+            listBox2.Items.Add("--------------------");
+        }
+
+    
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+
+
+            if (textBox1.Text.Length != 4)
+            { MessageBox.Show("4 Basamaklı Sayı Giriş Yapınız!"); }
+            else
+            {
+
+                if (sayi == 0)
+                { var randomSayi = RandomSayi(); }
+                else
+                { var randomSayi = sayi; }
+
+                string sayi2 = sayi.ToString();
+                char[] array = sayi2.ToCharArray();
+
+                char[] girilensayi = textBox1.Text.ToCharArray();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    char array2 = array[i];
+                    char girilensayi2 = girilensayi[i];
+
+                    if (array[i] == girilensayi[i])
+                    {
+                        listBox1.Items.Add((i + 1) + ". Harf Kontrol : +");
+
+                        j++;
+                        if (j == 4)
+                        {
+                            listBox1.Items.Add("Doğru Bildiniz");
+                        }
+
+                    }
+                    else
+                    {
+                        listBox1.Items.Add((i + 1) + ". Harf Kontrol : -");
+                    }
+                }
+                listBox1.Items.Add("--------------------");
+            }
+
+
+
+
+            RandomSayiB();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Numerik değer giriniz.");
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+    }
+}
